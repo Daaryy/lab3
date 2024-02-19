726be3ceb77ada1accae49910b84c57265152721
Daaryy <160476044+Daaryy@users.noreply.github.com>
2024-02-19 13:17:54 +0300

    Update Water.java

--- lab3/src/objects/Water.java
+++ lab3/src/objects/Water.java
@@ -4,7 +4,7 @@ public class Water extends NotLive{
     public Water(String name) {
         super(name);
     }
-    public void rising(Water water){
+    public void rising(){
         System.out.println(name +"кипит");
     }
 }
